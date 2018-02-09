f9fcc87e778acef734a951095ec851b844567445
Hamid Reza Ashrafnezhad <ashrafnezhad.hamidreza@gmail.com>
2018-02-09 15:52:28 +0330

    Update upload.py

--- upload.py
+++ upload.py
@@ -5,8 +5,8 @@ def main():
     site_url = 'http://hivoja.com'
     client_key = '483wsnlnvgllq4ywqpv50arrv20lr23b'
     client_secret = '86q8x8oewwhc7k6f33xx6ggd249eojif'
-    resource_owner_key = 'igahrcei0eka2snvinubqjhionocwld9'
-    resource_owner_secret = '25arnejde4oda4sxvma5eq81lgtjbx7j'
+    resource_owner_key = 'frshn9o8gmbkmxtjlv31fgskx3hbhbw0'
+    resource_owner_secret = 'iul4rgh0mq9vnd8ygnkuwxnnklu8gb35'
     auth = HeaderOAuth(client_key, client_secret, resource_owner_key, resource_owner_secret)
 
     sku = 'katooni hivoja -XL'
